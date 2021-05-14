dabf6f62d1a3e1381e11ebf7886b9ee78e5213c0
Gyula Madarasz <gyula.madarasz@gmail.com>
2021-05-14 20:45:53 +0100

    br fix

--- tpls/mails/account/reset.html.php
+++ tpls/mails/account/reset.html.php
@@ -1,3 +1,3 @@
-<strong>Pasword reset requested</strong>
+<strong>Pasword reset requested</strong><br />
 Use the following link to reset your pasword:<br />
 <a href="{$__base}reset?token={$token}">{$__base}reset?token={$token}</a>
