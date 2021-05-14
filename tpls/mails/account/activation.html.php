6bbc4079d2210f9b945dd629d3607b8fdc45c4bf
Gyula Madarasz <gyula.madarasz@gmail.com>
2021-05-14 20:38:05 +0100

    typo

--- tpls/mails/account/activation.html.php
+++ tpls/mails/account/activation.html.php
@@ -1,3 +1,3 @@
-<string>Activate your account</strong><br />
+<strong>Activate your account</strong><br />
 Use the following link to activate your account:<br />
 <a href="{$__base}activation?token={$token}">{$__base}activation?token={$token}</a>
